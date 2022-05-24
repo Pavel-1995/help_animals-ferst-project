a58b3e1cb2452b57e0157ab578f5aa6b7d5b4aac
Pavel Aliaksei <lexa.kun.11@mail.ru>
2022-05-24 13:03:42 +0300

    Add_static_file

--- Help_Animals/help_a/urls.py
+++ Help_Animals/help_a/urls.py
@@ -2,7 +2,9 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('transportation', views.transportation, name='transportation'),
+    path('connection', views.connection, name='connection'),
 
 ]
--- Help_Animals/help_a/views.py
+++ Help_Animals/help_a/views.py
@@ -8,3 +8,9 @@ def index(request):
 
 def about(request):
     return render(request, "help_a/about.html")
+
+def transportation(request):
+    return render(request, "help_a/transportation.html")
+
+def connection(request):
+    return render(request, "help_a/connection.html")
